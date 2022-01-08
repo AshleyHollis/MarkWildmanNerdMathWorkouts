87ff73d674e171bf0a02f768b9336aa03e8dae28
Ashley Hollis <ashleyhollis@outlook.com>
2022-01-08 18:05:29 +1000

    Add KettleBellSwings10SetsX20RepsStrategy

---
+++ MarkWildmanNerdMathWorkouts/MarkWildmanNerdMathWorkouts.Tests/KettleBellSwings10SetsX20RepsStrategyShould.cs
@@ -0,0 +1,86 @@
+using FluentAssertions;
+using MarkWildmanNerdMathWorkouts.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace MarkWildmanNerdMathWorkouts.Tests
+{
+    public class KettleBellSwings10SetsX20RepsStrategyShould
+    {
+        public static IEnumerable<object[]> GetTestData()
+        {
+            // Weight | Sets | Reps | Work Capacity
+            var allData = new List<object[]>
+            {
+                new object[] { 35, 70, new List<int>{35, 53, 70 }, string.Join(Environment.NewLine, new List<string> { 
+                        "35 10 10 3500",
+                        "35 10 11 3850",
+                        "35 10 12 4200",
+                        "35 10 13 4550",
+                        "35 10 14 4900",
+                        "35 10 15 5250",
+                        "35 10 16 5600",
+                        "35 10 17 5950",
+                        "35 10 18 6300",
+                        "35 10 19 6650",
+                        "35 10 20 7000",
+                        "53 10 10 5300",
+                        "53 10 11 5830",
+                        "53 10 12 6360",
+                        "53 10 13 6890",
+                        "53 10 14 7420",
+                        "53 10 15 7950",
+                        "53 10 16 8480",
+                        "53 10 17 9010",
+                        "53 10 18 9540",
+                        "53 10 19 10070",
+                        "53 10 20 10600",
+                        "70 10 10 7000",
+                        "70 10 11 7700",
+                        "70 10 12 8400",
+                        "70 10 13 9100",
+                        "70 10 14 9800",
+                        "70 10 15 10500",
+                        "70 10 16 11200",
+                        "70 10 17 11900",
+                        "70 10 18 12600",
+                        "70 10 19 13300",
+                        "70 10 20 14000"
+                    }
+                )}
+            };
+
+            return allData;
+        }
+
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void Test1(int startWeight, int targetWeight, List<int> availableWeights, string expected)
+        {
+            /*  Increase reps from 10 to 20 then increase weight.
+                Weights:  16kg (35lbs), 24kg (53lbs), 32kg (70lbs)
+               
+                Expectation:
+                Format: Weight x Sets x Reps = Work Capacity
+                35 x 10 x 10 = 3500
+                35 x 10 x 15 = 5250
+                35 x 10 x 20 = 7000
+                53 x 10 x 10 = 5300
+                53 x 10 x 15 = 7950
+                53 x 10 x 20 = 10600
+                70 x 10 x 10 = 7000
+                70 x 10 x 15 = 10500
+                70 x 10 x 20 = 14000
+
+                Is 10 min of Kettlebelling enough - Part II - Yes - Nerd Math
+                https://www.youtube.com/watch?v=lcECmuWTL3g
+            */
+            var kettleBellSwings10SetsX20RepsStrategy = new KettleBellSwings10SetsX20RepsStrategy(startWeight, targetWeight, availableWeights);
+            var workouts = kettleBellSwings10SetsX20RepsStrategy.Generate();
+
+            string.Join(Environment.NewLine, workouts.Select(x => x.ToString())).Should().Be(expected);
+        }
+    }
+}
---
+++ MarkWildmanNerdMathWorkouts/MarkWildmanNerdMathWorkouts/Shared/KettleBellSwings10SetsX20RepsStrategy.cs
@@ -0,0 +1,39 @@
+﻿using MarkWildmanNerdMathWorkouts.Shared.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkWildmanNerdMathWorkouts.Shared
+{
+    public class KettleBellSwings10SetsX20RepsStrategy
+    {
+        private int startWeight;
+        private int targetWeight;
+        private List<int> availableWeights;
+
+        public KettleBellSwings10SetsX20RepsStrategy(int startWeight, int targetWeight, List<int> availableWeights)
+        {
+            this.startWeight = startWeight;
+            this.targetWeight = targetWeight;
+            this.availableWeights = availableWeights;
+        }
+
+        public List<WorkPerformed> Generate()
+        {
+            var useableWeights = availableWeights.Where(x => x >= startWeight && x <= targetWeight).ToList();
+            var workouts = new List<WorkPerformed>();
+
+            foreach (var weight in useableWeights)
+            {
+                for (int i = 10; i <= 20; i++)
+                {
+                    workouts.Add(new WorkPerformed() { Weight = weight, Sets = 10, Reps = i });
+                }
+            }
+
+            return workouts;
+        }
+    }
+}
