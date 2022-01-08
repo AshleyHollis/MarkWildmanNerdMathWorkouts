a71cb5dee860eaa51bd71f15daad3bb7fd095f70
Ashley Hollis <ashleyhollis@outlook.com>
2022-01-08 20:04:44 +1000

    Add calculate next days of weeks logic

---
+++ MarkWildmanNerdMathWorkouts/MarkWildmanNerdMathWorkouts/Shared/Helpers/DateTimeExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkWildmanNerdMathWorkouts.Shared.Helpers
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime Next(this DateTime from, DayOfWeek dayOfTheWeek)
+        {
+            var date = from.Date.AddDays(1);
+            var days = ((int)dayOfTheWeek - (int)date.DayOfWeek + 7) % 7;
+            return date.AddDays(days);
+        }
+
+        public static IReadOnlyList<DateTime> Next(this DateTime from, params DayOfWeek[] days)
+        {
+            return Next(from, DateCalculationKind.And, days);
+        }
+
+        public static IReadOnlyList<DateTime> Next(this DateTime from, DateCalculationKind calculationKind, params DayOfWeek[] days)
+        {
+            return Next(from, calculationKind, 1, days);
+        }
+
+        public static IReadOnlyList<DateTime> Next(this DateTime from, DateCalculationKind calculationKind, int numberOfDaysRequiredPerDay, params DayOfWeek[] days)
+        {
+            if (days == null)
+                return Array.Empty<DateTime>();
+
+            DateTime? result = null;
+            var results = new List<DateTime>();
+
+            // And means, we don't want to duplicate data
+            days = calculationKind == DateCalculationKind.And
+                ? days.Distinct().ToArray()
+                : days;
+
+            var numberOfDaysRequired = numberOfDaysRequiredPerDay * days.Length;
+
+            while (results.Count < numberOfDaysRequired)
+            {
+                foreach (var dayOfWeek in days)
+                {
+                    result = calculationKind == DateCalculationKind.And || result is null
+                        ? Next(from, dayOfWeek)
+                        : Next(result.Value, dayOfWeek);
+
+                    results.Add(result.Value);
+                }
+            }
+
+            return results.Take(numberOfDaysRequired).ToList();
+        }
+    }
+}
